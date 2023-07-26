0a6a70e1c2c3a2214d8691e3bf336f9d6dc39b7a
vinihzets <viniciusdaarky@outlook.com>
2023-07-26 13:14:38 -0300

    finalizado chat

--- lib/auth/home_repository.dart
+++ lib/auth/home_repository.dart
@@ -3,8 +3,6 @@ import 'package:training/core/failure/failure.dart';
 import 'package:training/core/services/auth_service.dart';
 import 'package:training/core/services/database_service.dart';
 
-import '../model/message_model.dart';
-
 class HomeRepository {
   AuthService authService;
   DatabaseService databaseService;
@@ -13,19 +11,6 @@ class HomeRepository {
   Future<Either<Failure, void>> signOut() async {
     try {
       return Right(await authService.auth.signOut());
-    } on Exception catch (e) {
-      return Left(RemoteFailure(message: e.toString()));
-    }
-  }
-
-  Future<Either<Failure, List<MessageModel>>> getMessages() async {
-    try {
-      final request = await databaseService.chat.get();
-
-      List<MessageModel> listMessages =
-          request.docs.map((e) => MessageModel.fromJson(e.data())).toList();
-
-      return Right(listMessages);
     } on Exception catch (e) {
       return Left(RemoteFailure(message: e.toString()));
     }
--- lib/widgets/chat_screen.dart
+++ lib/widgets/chat_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 
 class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
   @override
   // ignore: library_private_types_in_public_api
   _ChatScreenState createState() => _ChatScreenState();
